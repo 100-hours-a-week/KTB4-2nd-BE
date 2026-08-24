a9da0a66dada239cd614e0a3b1c5e7bcfc3f9fd5
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-08-24 16:20:48 +0900

    init repository

---
+++ src/test/java/com/yeodam/yeodambe/YeodamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeodam.yeodambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeodamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
